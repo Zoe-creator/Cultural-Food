16ad72398781ddbdfbb301b5134eeed0d85ffa04
lizhenwen727 <59024841+lizhenwen727@users.noreply.github.com>
2021-02-22 21:48:15 -0500

    iphone test

--- cultural-food/src/components/form/Form.jsx
+++ cultural-food/src/components/form/Form.jsx
@@ -66,25 +66,25 @@ export default function Form(props) {
     <form onSubmit={handleSubmit} className="form">
       <h2> {params.id ? "Edit a Food" : "Submit a New Food"}</h2>
       <label htmlFor="name">Name: </label>
-      <input type="text" value={name} onChange={(e) => setName(e.target.value)} />
+      <input type="text" maxLength="30" placeholder="Limit of 30" value={name} onChange={(e) => setName(e.target.value)} />
       <br />
       <label htmlFor="Description of Food">Description of Food: </label>
-      <textarea type="text" value={description} onChange={(e) => setDecription(e.target.value)} ></textarea>
+      <textarea type="text" maxLength="300" placeholder="Limit of 300" value={description} onChange={(e) => setDecription(e.target.value)} ></textarea>
       <br />
       <label htmlFor="Origin of Food">Origin of Food: </label>
-      <input type="text" value={origin} onChange={(e) => setOrigin(e.target.value)} />
+      <input type="text" placeholder="City, Country" value={origin} onChange={(e) => setOrigin(e.target.value)} />
       <br />
       <label htmlFor="Valid Image Link">Valid Image Link: </label>
-      <input type="text" value={pictureURL} onChange={(e) => setPictureURL(e.target.value)} />
+      <input type="text" placeholder="http://"  value={pictureURL} onChange={(e) => setPictureURL(e.target.value)} />
       <br />
       <label htmlFor="calories of Food">calories of Food: </label>
-      <input type="number" value={calories} onChange={(e) => setCalories(e.target.valueAsNumber)} />
+      <input type="number" placeholder="Valid Number " value={calories} onChange={(e) => setCalories(e.target.valueAsNumber)} />
       <br />
       <label htmlFor="fat of Food">Fat of Food: </label>
-      <input type="number" value={fat} onChange={(e) => setFat(e.target.valueAsNumber)} />
+      <input type="number"  placeholder="Valid Number" value={fat} onChange={(e) => setFat(e.target.valueAsNumber)} />
       <br />
       <label htmlFor="Carbs of Food">carbs of Food: </label>
-      <input type="number" value={carbs} onChange={(e) => setCarbs(e.target.valueAsNumber)} />
+      <input type="number" placeholder="Valid Number" value={carbs} onChange={(e) => setCarbs(e.target.valueAsNumber)} />
       <br />
       <button type="submit">{params.id ? "Edit" : "Add Me"}</button>
     </form>
